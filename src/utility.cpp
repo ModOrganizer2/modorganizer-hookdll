093e37939d5ba6aa84fb8df7cde33f29078a5f88
Tannin <devnull@localhost>
2015-01-29 19:11:43 +0100

    bugfix: PathStartsWith detection failed if  substring ended on (back-)slash

--- src/utility.cpp
+++ src/utility.cpp
@@ -71,17 +71,20 @@ bool PathStartsWith(LPCSTR string, LPCSTR subString)
 
 bool PathStartsWith(LPCWSTR string, LPCWSTR subString)
 {
-  size_t len = wcslen(subString);
-  if (wcslen(string) < len) {
-    return false;
-  }
-
-  for (size_t i = 0; i < len; ++i) {
-    if (towlower(string[i]) != towlower(subString[i])) {
+  while ((*string != L'\0') && (*subString != L'\0')) {
+    if (towlower(*(string++)) != towlower(*(subString++))) {
       return false;
     }
   }
-  return (string[len] == L'\0') || (string[len] == L'/') || (string[len] == L'\\');
+
+  if ((*string == L'\0') && (*subString != L'\0')) {
+    // string shorter than substring
+    return false;
+  }
+
+  // true if string == substring or if string is now at a path separator OR if substring ended on a path separator (which was matched by string)
+  return (*string == L'\0') || (*string == L'/') || (*string == L'\\')
+      || (*(subString - 1) == L'/') || (*(subString - 1) == L'/');
 }
 
 bool StartsWith(LPCSTR string, LPCSTR subString)
